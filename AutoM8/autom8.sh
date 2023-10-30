bc633d38ff59263cdda6989bf53b513356900740
Marcio Moreira Jr <mdmjunior@gmail.com>
2023-10-29 23:16:36 -0300

    Kickoff

---
+++ AutoM8/autom8.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#############################################################################
+# AutoM8 - Automated Linux Install Tool
+# https://autom8.worldwidelabs.com.br
+# Author         : Marcio Moreira Junior
+# Email          : mdmjunior@gmail.com
+# Date           : 2023/10/29
+# Version        : 1.0    
+#############################################################################
+
+
